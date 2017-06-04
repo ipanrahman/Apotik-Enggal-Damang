0d21715dcda21d1831037286e0c9f43eaa18cfeb
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-06-04 15:37:19 +0700

    database config postgres heroku

---
+++ src/main/java/com/github/ipan97/enggal/damang/DatabaseConfig.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Ipan Taupik Rahman
+ */
+
+package com.github.ipan97.enggal.damang;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by ipan on 04/06/17.
+ */
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
